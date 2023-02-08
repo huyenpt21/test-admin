0910dce85c774f2e6b75035f4b012e90f83a4a41
HuyenPT <huyenpthe140045@fpt.edu.vn>
2023-02-09 00:21:59 +0700

    add setting module

--- src/pages/settings/index.js
+++ src/pages/settings/index.js
@@ -1,5 +1,74 @@
-import React from "react";
+import { Button, Col, DatePicker, Form, Input, Row } from "antd";
+import React, { useState } from "react";
 
 export default function Settings() {
-  return <div>Settings</div>;
+  const [colorPicked, setColorPicked] = useState();
+  const [formSetting] = Form.useForm();
+  const handleChangeColor = (e) => {
+    setColorPicked(e.target.value);
+  };
+  const handleSubmitForm = (value) => {
+    console.log(value);
+  };
+  return (
+    <div className="container">
+      <h1>Settings</h1>
+      <Form
+        form={formSetting}
+        name="settings"
+        layout="vertical"
+        onFinish={handleSubmitForm}
+      >
+        <Row gutter={32}>
+          <Col span={6}>
+            <Form.Item label="Title" name="title">
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item label="Email" name="email">
+              <Input type="email" />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={32}>
+          <Col span={6}>
+            <Form.Item label="Background Color" name="backgroundColor">
+              <Input style={{ color: colorPicked }} />
+            </Form.Item>
+          </Col>
+          <Col span={2}>
+            <Form.Item label="Color" name="color">
+              <Input type="color" onChange={handleChangeColor} />
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item label="Active Date" name="activeDate">
+              <DatePicker />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={32}>
+          <Form.Item shouldUpdate style={{ marginLeft: "16px" }}>
+            {() => (
+              <Button
+                type="primary"
+                htmlType="submit"
+                disabled={
+                  !formSetting.isFieldsTouched([
+                    "title",
+                    "email",
+                    "backgroundColor",
+                    "activeDate",
+                  ])
+                }
+              >
+                Save
+              </Button>
+            )}
+          </Form.Item>
+        </Row>
+      </Form>
+    </div>
+  );
 }
