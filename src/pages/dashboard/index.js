4254380aae97564665576e8e08dd1084b39d7104
HuyenPT <huyenpthe140045@fpt.edu.vn>
2023-02-08 17:58:08 +0700

    update highlight button

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,20 +1,38 @@
-import { Button, Row } from "antd";
-import React from "react";
+import { Button, Col, Row } from "antd";
+import React, { useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import { revenuePath, subscriptionPath } from "../../constant/common";
+import styles from "./index.module.css";
 
 export default function Dashboard() {
   const navigate = useNavigate();
+  const [menuType, setMenuType] = useState(1);
   const handleNavigate = (path) => {
     navigate(path);
   };
   return (
     <>
+      <h1>Dashboard</h1>
       <Row>
-        <Button onClick={() => handleNavigate(subscriptionPath)}>
+        <Button
+          type={menuType === 1 ? "primary" : "default"}
+          onClick={() => {
+            handleNavigate(subscriptionPath);
+            setMenuType(1);
+          }}
+          className={styles.btn}
+        >
           Subcription
         </Button>
-        <Button onClick={() => handleNavigate(revenuePath)}>Revenue</Button>
+        <Button
+          type={menuType === 2 ? "primary" : "default"}
+          onClick={() => {
+            handleNavigate(revenuePath);
+            setMenuType(2);
+          }}
+        >
+          Revenue
+        </Button>
       </Row>
       <div style={{ marginTop: "20px" }}>
         <Outlet />
